0ee7464f878007c76dc639416e405f7d98469c01
ManamiMayoki <manamimarufa@gmail.com>
2026-06-27 22:25:07 +0600

    new

--- TS_01/src/index.ts
+++
@@ -1,7 +0,0 @@
-let learnerName: string = "Marufa";
-let practiceHour: number = 2;
-
-function displayProgress(name: string,hours: number): void{
-    console.log('Hello ' + name + ', you have completed ' + hours + 'hours of practice today!');
-}
-displayProgress(learnerName,practiceHour);
