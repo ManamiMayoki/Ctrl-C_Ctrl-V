ad2f8a48b089eb22731054ca6871f6b9cae31a8c
ManamiMayoki <manamimarufa@gmail.com>
2026-06-29 00:08:10 +0600

    backtickets

--- Weather App/app.js
+++ Weather App/app.js
@@ -1,7 +1,9 @@
-const url="https://api.openweathermap.org/data/2.5/weather?";
-const apiKey="2384797dde82a98f0c48c207d447b336";
+const url = "https://api.openweathermap.org/data/2.5/weather?";
+const apiKey = "2384797dde82a98f0c48c207d447b336";
 
 async function getWeatherData(city){
-    let finalUrl='${url}q=${city}&appid=${apiKey}';
-    let weatherData=await fetch(finalUrl);
+    let finalUrl = `${url}q=${city}&appid=${apiKey}`;
+    
+    let weatherData = await fetch(finalUrl);
+    console.log(weatherData);
 }
