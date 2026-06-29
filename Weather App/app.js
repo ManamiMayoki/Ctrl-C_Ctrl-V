0b8e71cea85d86ddbe947b8ccd2211dae229d51a
ManamiMayoki <manamimarufa@gmail.com>
2026-06-29 14:13:16 +0600

    bug

--- Weather App/app.js
+++ Weather App/app.js
@@ -1,23 +1,29 @@
-const startbtn=document.querySelector(".start");
-const search=document.querySelector("#inputfield");
+const startbtn=document.querySelector(".startbtn");
+const search=document.querySelector("#inputField");
 const searchIcon=document.querySelector("#searchIcon");
 const desc=document.querySelector("#description");
 const temp=document.querySelector("#temp");
-const city=document.querySelector("city");
+const city=document.querySelector("#city");
 const wind=document.querySelector("#windSpeed");
 const humidity=document.querySelector("#humidity");
-const goHome=document.querySelector(".homwbtn");
+const goHome=document.querySelector(".homebtn");
 const icon=document.querySelector("#icon");
 const mainBox1=document.querySelector(".mainBox1");
 const mainBox2=document.querySelector(".mainBox2");
 const mainBox3=document.querySelector(".mainBox3");
 
 
+startbtn.addEventListener("click",()=>{
+    mainBox1.classList.add("inactive");
+    mainBox2.classList.remove("inactive");
+})
+
+
 const url = "https://api.openweathermap.org/data/2.5/weather?";
 const apiKey = "2384797dde82a98f0c48c207d447b336";
 
 async function getWeatherData(city){
-    let finalUrl = `${url}q=${city}&appid=${apiKey}`;
+    let finalUrl = `${url}q=${city}&appid=${apiKey}&units=metric`;
     let response=await fetch(finalUrl);
     let weatherData = await response.json();
     console.log(weatherData);
