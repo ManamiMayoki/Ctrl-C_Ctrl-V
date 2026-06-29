fa206d69efa69022f23630b79192dd163991cb7e
ManamiMayoki <manamimarufa@gmail.com>
2026-06-29 13:48:09 +0600

    app

--- Weather App/app.js
+++ Weather App/app.js
@@ -1,3 +1,16 @@
+const startbtn=document.querySelector(".start");
+const search=document.querySelector("#inputfield");
+const searchIcon=document.querySelector("#searchIcon");
+const desc=document.querySelector("#description");
+const temp=document.querySelector("#temp");
+const city=document.querySelector("city");
+const wind=document.querySelector("#windSpeed");
+const humidity=document.querySelector("#humidity");
+const goHome=document.querySelector(".homwbtn");
+const icon=document.querySelector("#icon");
+
+
+
 const url = "https://api.openweathermap.org/data/2.5/weather?";
 const apiKey = "2384797dde82a98f0c48c207d447b336";
 
