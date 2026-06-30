4dcc85c31f888a189228f26e99c4d41abaee8476
ManamiMayoki <manamimarufa@gmail.com>
2026-06-30 23:22:04 +0600

    box3 active,box 2 inactive

--- Weather App/app.js
+++ Weather App/app.js
@@ -1,34 +1,33 @@
-const startbtn=document.querySelector(".startbtn");
-const search=document.querySelector("#inputField");
-const searchIcon=document.querySelector("#searchIcon");
-const desc=document.querySelector("#description");
-const temp=document.querySelector("#temp");
-const city=document.querySelector("#city");
-const wind=document.querySelector("#windSpeed");
-const humidity=document.querySelector("#humidity");
-const goHome=document.querySelector(".homebtn");
-const icon=document.querySelector("#icon");
-const mainBox1=document.querySelector(".mainBox1");
-const mainBox2=document.querySelector(".mainBox2");
-const mainBox3=document.querySelector(".mainBox3");
+const startbtn = document.querySelector(".startbtn");
+const search = document.querySelector("#inputField");
+const searchIcon = document.querySelector("#searchIcon");
+const desc = document.querySelector("#description");
+const temp = document.querySelector("#temp");
+const city = document.querySelector("#city");
+const wind = document.querySelector("#windSpeed");
+const humidity = document.querySelector("#humidity");
+const goHome = document.querySelector(".homebtn");
+const icon = document.querySelector("#icon");
+const mainBox1 = document.querySelector(".mainBox1");
+const mainBox2 = document.querySelector(".mainBox2");
+const mainBox3 = document.querySelector(".mainBox3");
 
-
-startbtn.addEventListener("click",()=>{
-    mainBox1.classList.add("inactive");
-    mainBox2.classList.remove("inactive");
-})
-
+startbtn.addEventListener("click", () => {
+  mainBox1.classList.add("inactive");
+  mainBox2.classList.remove("inactive");
+});
 
 const url = "https://api.openweathermap.org/data/2.5/weather?";
 const apiKey = "2384797dde82a98f0c48c207d447b336";
 
-async function getWeatherData(city){
-    let finalUrl = `${url}q=${city}&appid=${apiKey}&units=metric`;
-    let response=await fetch(finalUrl);
-    let weatherData = await response.json();
-    console.log(weatherData);
+async function getWeatherData(city) {
+  let finalUrl = `${url}q=${city}&appid=${apiKey}&units=metric`;
+  let response = await fetch(finalUrl);
+  let weatherData = await response.json();
+  console.log(weatherData);
 
-    if(weatherData.cod ==404){
-        main
-    }
+  if (weatherData.cod == 404) {
+    mainBox2.classList.add("inactive");
+    mainBox3.classList.remove("inactive");
+  }
 }
